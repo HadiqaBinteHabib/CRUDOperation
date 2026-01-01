615e5e292df2dcfbc17e13270354f47f47c7cb31
HadiqaBinteHabib <bintehabibhadiqa@gmail.com>
2026-01-01 19:26:03 +0500

    changes added

--- CRUDOperation/Models/Category.cs
+++ CRUDOperation/Models/Category.cs
@@ -7,9 +7,13 @@ namespace CRUDOperation.Models
         [Key]
         public int CategoryId { get; set; }
 
+        [Required(ErrorMessage = "Name is required")]
+
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "Description is required")]
         public string Description { get; set; }
+
 
         public virtual List<Product> Products { get; set; }
 
